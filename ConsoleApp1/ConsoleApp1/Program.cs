05922d9f5e301ef88eb84d794b06b3726d3fbb68
aamir <wasi3aamir@gmail.com>
2019-02-07 00:37:00 +0600

    Accepted

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCP_254
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 1; t <= T; t++)
+            {
+                string line = Console.ReadLine();
+                string[] space = line.Split('\n');
+                string res = InvertCasing(line);
+                Console.WriteLine($"{res}");
+            }
+        }
+        static string InvertCasing(string s)
+        {
+            char[] c = s.ToCharArray();
+            char[] cUpper = s.ToUpper().ToCharArray();
+            char[] cLower = s.ToLower().ToCharArray();
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] == cUpper[i])
+                {
+                    c[i] = cLower[i];
+                }
+                else
+                {
+                    c[i] = cUpper[i];
+                }
+            }
+            return new string(c);
+        }
+    }
+}
